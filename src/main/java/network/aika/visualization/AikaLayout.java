d62bb3b2cf104a717700a823bed41386cf83f971
lukas <lukas.molzberger@gmx.de>
2021-01-02 07:28:23 +0100

    Initial commit

---
+++ src/main/java/network/aika/visualization/AikaLayout.java
@@ -0,0 +1,37 @@
+package network.aika.visualization;
+
+import network.aika.neuron.activation.Activation;
+import network.aika.text.Document;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.ui.layout.springbox.NodeParticle;
+import org.graphstream.ui.layout.springbox.implementations.SpringBox;
+
+public class AikaLayout extends SpringBox {
+
+    Document doc;
+    Graph graph;
+
+    AikaLayout(Document doc, Graph g) {
+        this.doc = doc;
+        this.graph = g;
+    }
+
+    @Override
+    public String getLayoutAlgorithmName() {
+        return "AikaLayout";
+    }
+
+    @Override
+    protected void chooseNodePosition(NodeParticle n0, NodeParticle n1) {
+        super.chooseNodePosition(n0, n1);
+    }
+
+    @Override
+    public NodeParticle newNodeParticle(String id) {
+        Node n = graph.getNode(id);
+        Activation act = doc.getActivation(n.getAttribute("aika.id", Integer.class));
+
+        return new AikaParticle(act, this, id);
+    }
+}
