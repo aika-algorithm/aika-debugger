5408295bdafa68c3758165ccbfc118b9877b348f
lukas <lukas.molzberger@gmx.de>
2021-07-06 09:29:17 +0200

    scopes

--- src/main/java/network/aika/debugger/scopes/ScopesViewManager.java
+++ src/main/java/network/aika/debugger/scopes/ScopesViewManager.java
@@ -2,13 +2,19 @@ package network.aika.debugger.scopes;
 
 import network.aika.Model;
 import network.aika.debugger.AbstractViewManager;
+import network.aika.neuron.activation.scopes.Scope;
+import network.aika.neuron.activation.scopes.Transition;
+import org.graphstream.graph.Node;
 import org.graphstream.ui.graphicGraph.GraphicElement;
 
 import javax.swing.*;
+import java.util.function.Consumer;
 
 public class ScopesViewManager extends AbstractViewManager<ScopesConsole, ScopesGraphManager> {
 
     private Model model;
+
+    private ScopesConsole console = new ScopesConsole();
 
     public ScopesViewManager(Model model) {
         this.model = model;
@@ -23,9 +29,30 @@ public class ScopesViewManager extends AbstractViewManager<ScopesConsole, Scopes
 
     }
 
+
+    private void drawTransition(Transition t) {
+
+    }
+
+    private void drawScope(Scope s, double x, double y) {
+        graphManager.lookupNode(s,
+                node -> {
+                    drawScope(s, x, y, node);
+                });
+    }
+
+    private void drawScope(Scope s, double x, double y, Node node) {
+        node.setAttribute("aika.scopeId", s.getId());
+
+        node.setAttribute("x", x);
+        node.setAttribute("y", y);
+
+        node.setAttribute("ui.label", s.getLabel());
+    }
+
     @Override
     public JComponent getConsolePane() {
-        return null;
+        return console;
     }
 
     @Override
@@ -34,18 +61,14 @@ public class ScopesViewManager extends AbstractViewManager<ScopesConsole, Scopes
     }
 
     public void initScopes() {
- /*       getModel()
-                .getTemplates()
+        getModel()
                 .getScopes()
-                .forEach(tn -> drawNeuron(tn, tn.getTemplateInfo().getXCoord(), tn.getTemplateInfo().getYCoord()));
+                .getScopes()
+                .forEach(s -> drawScope(s, s.getXCoord(), s.getYCoord()));
 
         getModel()
-                .getTemplates()
-                .getAllTemplates()
-                .forEach(tn -> {
-                    drawInputSynapses(tn);
-                });
-
-  */
+                .getScopes()
+                .getTransitions()
+                .forEach(t -> drawTransition(t));
     }
 }
